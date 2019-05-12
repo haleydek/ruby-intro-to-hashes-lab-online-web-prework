071112ed93da8d4d1e5f44742ac0d7bc6e54eb80
Haley DeKeyser <hcd09@live.com>
2019-05-12 16:59:10 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,9 +14,8 @@ end
 
 def monopoly_with_second_tier
 	monopoly = {}
-	monopoly[:railroads] = {}
+	monopoly[:railroads] = { pieces: 4, names: {}, rent_in_dollars: {} }
 	monopoly
-	
 end
 
 def monopoly_with_third_tier
